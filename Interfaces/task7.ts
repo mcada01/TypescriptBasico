e2a5b0604f605d0435ede8d1c0c6b110b48c7b84
mcada01 <Melissa.Cadavid@ifb-group.com>
2019-11-22 17:24:28 -0500

    espacios

--- Interfaces/task7.ts
+++ Interfaces/task7.ts
@@ -6,7 +6,7 @@ interface Book{
 }
 
 var mybook:Book={
-    title:"Cien años de Soledad",
+    title:"Cien años de Soledad ",
     isbn:"Si",
     description:"Novela de Gabriel García Marquez",
     price:5000
